d1a84a2319a6ce6b30315db0b5266b3e6c80e3e4
cholae000 <cholaelaehlaing000000@gmail.com>
2023-03-14 23:39:48 +0630

    assignment 8+9

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "time.h"
+#include "assignment9.h"
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+    return 0;
+}
+
